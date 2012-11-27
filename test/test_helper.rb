3703e4689e8598e4c00417f2776b99687393574b
Gerry Power <gerry@zoocasa.com>
2012-11-26 20:29:54 -0500

    Don't include the MiniTest:Reporters for non 1.9 ruby

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,6 @@ require 'minitest/reporters'
 require 'rails'
 require File.expand_path('../../lib/webhookr-mandrill.rb', __FILE__)
 
-MiniTest::Reporters.use!(MiniTest::Reporters::SpecReporter.new)
+if RUBY_VERSION >= "1.9"
+  MiniTest::Reporters.use!(MiniTest::Reporters::SpecReporter.new)
+end
